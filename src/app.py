453b400ea419b30dab1eddf9ae1b616160b84092
Gabriela Zambrano Rodríguez <102486455+gabrielazaro@users.noreply.github.com>
2023-05-19 19:00:49 +0000

    logrado

--- src/app.py
+++ src/app.py
@@ -2,13 +2,14 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
+import string
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Planet, People, FavoritePlanet, FavoritePeople
 #from models import Person
 
 app = Flask(__name__)
@@ -36,6 +37,41 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+@app.route('/search', methods=['GET'])
+def get_form():
+    return '''
+        <form action="/search">
+            <label for="planet_id">Planet ID:</label>
+            <input type="number" id="planet_id" name="planet_id">
+            <input type="submit" value="Search">
+        </form>
+        '''
+@app.route('/word-size/<string:word>')
+def get_size(word):
+    return str(len(word))
+    
+@app.route('/planet', methods=['POST'])
+def post_planet():
+    body = request.get_json()
+    print(body)
+    planet_name = body['planet_name']
+    population = body['population']
+    planet = Planet(planet_name=planet_name, population=population)
+    db.session.add(planet)
+    db.session.commit()
+    return 'Planeta agregado'
+
+@app.route('/people', methods=['POST'])
+def post_people():
+    body = request.get_json()
+    print(body)
+    people_name = body['people_name']
+    gender = body['gender']
+    new_people = People(people_name=people_name, gender=gender)
+    db.session.add(new_people)
+    db.session.commit()
+    return 'Personaje agregado'
+
 @app.route('/user', methods=['GET'])
 def handle_hello():
 
@@ -44,6 +80,40 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@app.route('/favorite/user/<int:user_id>/planet/<int:planet_id>', methods=['POST'])
+def get_favorites(user_id, planet_id):
+    new_favorites = FavoritePlanet(user_id=user_id, planet_id=planet_id)
+    db.session.add(new_favorites)
+    db.session.commit()
+    response_body = {'msg': 'favorito agregado correctamente'}
+    return jsonify(response_body)
+
+@app.route('/favorite/user/<int:user_id>/people/<int:people_id>', methods=['POST'])
+def get_favorites_people(user_id, people_id):
+    new_favorites = FavoritePeople(user_id=user_id, people_id=people_id)
+    db.session.add(new_favorites)
+    db.session.commit()
+    response_body = {'msg': 'favorito agregado correctamente'}
+    return jsonify(response_body)
+
+@app.route('/favorite/user/<int:user_id>/people/<int:people_id>', methods=['DELETE'])
+def delete_favorite_people(user_id, people_id):
+    favorite = FavoritePeople.query.filter_by(user_id=user_id,people_id=people_id).first()
+    db.session.delete(favorite)
+    db.session.commit()
+    response_body = {'msg': 'favorito borrado correctamente'}
+    return jsonify(response_body)
+
+@app.route('/favorite/user/<int:user_id>/planet/<int:planet_id>', methods=['DELETE'])
+def delete_favorite_planet(user_id, planet_id):
+    favorite = FavoritePlanet.query.filter_by(user_id=user_id,planet_id=planet_id).first()
+    db.session.delete(favorite)
+    db.session.commit()
+    response_body = {'msg': 'favorito borrado correctamente'}
+    return jsonify(response_body)
+    
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
